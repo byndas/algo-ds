bf7589a44ba9c69707d39625f3e8878d0dd22738
Marc Byndas <byndasma@gmail.com>
2017-10-26 23:23:12 +0000

    trees

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -38,30 +38,72 @@ Given a dictionary, create a prefix tree (commonly known as a trie)
 https://en.wikipedia.org/wiki/Trie
 
 */
-
+// N-ary Tree (any number of children)
 function Tree (value) {
-  // implement me...
+  this.value = value;
+  this.children = [];
 }
 
+
+// Adds child to tree or subtree bound to 'this'
 Tree.prototype.addChild = function(value) {
-  // implement me...
-};
-// Time complexity:
+  var child = new Tree(value);
+  this.children.push(child);
+  return child;
+}; // O(1)
+
+
+var tree = new Tree(1);
+var branch1 = tree.addChild(2);
+var branch2 = tree.addChild(3);
+var branch3 = tree.addChild(4);
+branch1.addChild(5);
+branch1.addChild(6);
+branch3.addChild(7).addChild(8);
 
 
 Tree.prototype.contains = function(value) {
-  // implement me...
-};
-// Time complexity:
+  if (this.value === value) return true;
+  for (var i=0; i<this.children.length; i++) {
+    if (this.children[i].contains(value)) return true;
+  }
+  return false;
+}; // O(n)
+
 
 
 Tree.prototype.traverseDepthFirst = function(fn) {
-  // implement me...
-};
-// Time complexity:
+  this.children.forEach(function(child) {
+    child.traverseDepthFirst(fn);
+  });
+  fn(this);
+} // O(n)
+
+
+var depthFirstResult = [];
+
+tree.traverseDepthFirst(function(node) {
+  depthFirstResult.push(node.value);
+});
+
+console.log(depthFirstResult, 'should be [5,6,2,3,8,7,4,1]');
 
 
 Tree.prototype.traverseBreadthFirst = function(fn) {
-  // implement me...
-};
-// Time complexity:
+  var queue = [this];
+  while (queue.length) {
+    var node = queue.shift();
+    fn(node.value);
+    node.children.forEach(function(child) {
+      queue.push(child);
+    });
+  }
+}; // O(n)
+
+var breadthFirstResult = [];
+
+tree.traverseBreadthFirst(function(node) {
+  breadthFirstResult.push(node);
+});
+
+console.log(breadthFirstResult, 'should be [1,2,3,4,5,6,7,8]');
