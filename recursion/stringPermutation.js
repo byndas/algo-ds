ce5aafd4f3d52a0822b6dd9326028c5a0e9eb6e1
Marc Byndas <byndasma@gmail.com>
2017-10-29 20:43:49 +0000

    string permutations

--- recursion/stringPermutation.js
+++ recursion/stringPermutation.js
@@ -1,3 +1,18 @@
 /*
-Write a function that takes a string and returns all permutations of the string. Ensure that there are no duplicates in the output.
+Write a function that takes a string and returns all permutations
+of the string. Ensure that there are no duplicates in the output.
 */
+
+function permutations(str) {
+  let results = {};
+  function recurse(word, remainder) {
+    if (remainder.length === 0) {
+      return results[word] = true;
+    }
+    for (let i = 0; i < remainder.length; i++) {
+      recurse(word + remainder[i], remainder.substr(0, i) + remainder.substr(i + 1));
+    }
+  }
+  recurse('', str);
+  return Object.keys(results);
+}
