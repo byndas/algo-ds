14ecde8afacf1bf5295586b04d70059b8fc34a26
Marc Byndas <byndasma@gmail.com>
2017-11-08 23:58:35 +0000

    doublyLinkedList

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -1,53 +1,61 @@
 /*
-BINARY SEARCH TREES
+BINARY SEARCH TREE
 
-Abstract data type
-A binary search tree is a tree with the additional constraints:
+An abstract data type, a tree with the additional constraints:
+
 - each node has only two child nodes (node.left and node.right)
-- all the values in the left subtree of a node are less than or equal to the value of the node
-- all the values in the right subtree of a node are greater than the value of the node
+- all left child node values <= parent node value
+- all right child node values > parent node value
 
 *** Operations:
 
 bsTree.insert(value)
-=> bsTree (return for chaining purposes)
-Insert value into correct position within tree
+=> returns bsTree ---> (for chaining purposes)
+Inserts value into correct position within tree
 
 bsTree.contains(value)
-=> true/false
-Return true if value is in tree, false if not
+=> returns true or false... if value is in tree
 
 bsTree.traverseDepthFirst_inOrder(callback)
-=> undefined
-Invoke the callback for every node in a depth-first in-order (visit left branch, then current node, than right branch)
-Note: In-Order traversal is most common type for binary trees. For binary search tree, this visits the nodes in ascending order (hence the name).
+=> return undefined
+Runs callback for each node in a depth-first in-order
+(left branch, then current node, then right branch)
+
+Note: In-Order traversal is most common type for binary trees.
+      For bsTrees, visits nodes in ascending order (hence the name).
 
 bsTree.traverseDepthFirst_preOrder(callback)
-=> undefined
-Invoke the callback for every node in a depth-first pre-order (visits current node before its child nodes)
+=> returns undefined
+Runs callback for each node in a depth-first pre-order 
+(visits current node before its child nodes)
 
 bsTree.traverseDepthFirst_postOrder(callback)
-=> undefined
-Invoke the callback for every node in a depth-first post-order (visit the current node after its child nodes)
+=> returns undefined
+Runs callback for each node in a depth-first post-order
+(visit the current node after its child nodes)
 
 bsTree.isValid()
-=> returns true if BST is a valid BST otherwise returns false. This method is useful for checking your other methods.
+=> returns true or false... if a valid BST
+Useful for checking your other methods
 
 bsTree.removeNode(value)
-=> node
-Remove node from tree.
+=> removes node from tree & returns it
 
 bsTree.checkIfFull()
-=> true/false
-A binary tree is full if every node has either zero or two children (no nodes have only one child)
+=> returns true or false
+A full binary tree has no nodes with only one child (only two or zero)
 
 bsTree.checkIfBalanced()
-=> true/false
-For this exercise, let's say that a tree is balanced if the minimum height and the maximum height differ by no more than 1. The height for a branch is the number of levels below the root.
+=> returns true or false
+A tree is balanced if (min height - max height differ) < 2. 
+The branch height === number of levels below the root.
 
 
-*** Additional Exercises:
-A binary search tree was created by iterating over an array and inserting each element into the tree. Given a binary search tree with no duplicates, how many different arrays would result in the creation of this tree.
+*** Additional Exercise:
+
+To make a bsTree: loop over an array & put each element into the tree.
+
+Given a bsTree with no duplicates, how many different arrays result?
 
 */
 
