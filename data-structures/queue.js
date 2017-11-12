b0656a0de7e19be3c3b467eae39113982b729379
Marc Byndas <byndasma@gmail.com>
2017-11-12 20:04:06 +0000

    queue

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -16,30 +16,28 @@ myQueue.dequeue()
 => removes & returns oldest element in collection
 
 myQueue.peek()
-=> oldest element added collection
-Similiar to dequeue, but do not remove element from collection
+=> returns oldest element in collection (not removing it)
 
 myQueue.count()
-=> number of elements in queue
+=> returns # of elements in queue
 
 
 *** Additional Exercises:
 
-Modify your queue to take a max capacity and return a string if you
-try to add an element when there's no more room:
-myQueue.enqueue(value)
-=> "Max capacity already reached. Remove element before adding a new one."
+Modify your queue to take a max capacity and return a string if adding 
+an element when no more room:
+  myQueue.enqueue(value) => "Max capacity already reached. Remove element before adding a new one."
 
-Create a contains method to check if a value is in the queue:
-myQueue.contains('findme')
-=> true/false
-What's the time complexity?
 
-Create an until method to get the number of dequeues until you get to a certain value:
-queue values - (first)2-5-7-3-6-9(last)
-myQueue.until(7)
-=> 3
-What's the time complexity?
+Write a 'contains' method checking if a value is in the queue:
+  myQueue.contains('findme') => true/false
+  Time complexity?
+
+
+Write an 'until' method getting # of dequeues until a given value:
+  queue values ---> (first)2-5-7-3-6-9(last)
+  myQueue.until(7) => 3
+  Time complexity?
 */
 
 function Queue(capacity) {
@@ -100,12 +98,12 @@ Queue.prototype.until = function(value) {
 /*
 *** Exercises:
 
-1. Implement a queue using two stacks.
+1. Write a queue using two stacks.
 
-2. Implement a double-ended queue, with the following methods: 
+2. Write a double-ended queue, with these methods: 
     enqueueLeft, dequeueLeft, enqueueRight, dequeueRight.
 
-3. Given a tree, print out the value of each node in breadth-first 
+3. Given a tree, print the value of each node in breadth-first 
     order using a queue data structure.
 
  */
