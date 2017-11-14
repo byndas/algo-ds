81f558398d34cc495b5d1f09d2a6da1bfc670881
Marc Byndas <byndasma@gmail.com>
2017-11-14 19:52:42 +0000

    stack

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -78,28 +78,27 @@ Stack.prototype.count = function() {
 /*
 *** Exercises:
 
-1. Implement a stack with a min method which returns the minimum
- element currently in the stack. This method should have O(1) time 
- complexity. Make sure your implementation handles duplicates.
+1. Implement a stack with a min method that returns the minimum
+ element currently in the stack...with O(1) & handles duplicates.
 
-2. Sort a stack so that its elements are in ascending order.
+2. Sort a stack's elements in ascending order.
 
 3. Given a string, is the parenthesis in the string balanced?
 Ex: balancedParens( 'sqrt(5*(3+8)/(4-2))' ) => true
 Ex: balancedParens( 'Math.min(5,(6-3))(' ) => false
 
-4. Towers of Hanoi - https://en.wikipedia.org/wiki/Tower_of_Hanoi
-You are given three towers (stacks) and N disks, each of different
-size. You can move the disks according to three constraints:
-   1. only one disk can be moved at a time
-   2. when moving a disk, you can only use pop (remove the top element) 
-and push (add to the top of a stack)
-   3. no disk can be placed on top of a disk that is smaller than it
-The disks begin on tower#1. Write a function that will move the disks
-from tower#1 to tower#3 in such a way that none of the constraints are violated.
+4. Towers of Hanoi --> https://en.wikipedia.org/wiki/Tower_of_Hanoi
+-- Given three towers (stacks) and N disks, each of different
+size... with three limitations:
+   1. may only move one disk at a time.
+   2. may only use pop (removes top element) & push (adds to the top).
+   3. no disk may be placed on top of a smaller disk.
+Disks begin on tower#1. 
+-- Write a function that moves the disks from tower#1 to tower#3.
  */
 
-// Implement a min stack
+
+// Write a min stack
 function MinStack(capacity) {
   this._capacity = capacity;
   this._storage = {};
