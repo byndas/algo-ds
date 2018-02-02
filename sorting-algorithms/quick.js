7cc0af2c864313982dc8b4eccb1cbdba9c933257
Marc Byndas <byndasma@gmail.com>
2018-02-02 00:53:44 +0000

    quick.js

--- sorting-algorithms/quick.js
+++ sorting-algorithms/quick.js
@@ -1,10 +1,20 @@
 /*
 MERGE SORT
+
 *** Description
 Like merge sort, quick sort employs a divide and conquer strategy.
-It has a partitioning step, in which you pick an element (called a pivot) and partition the array so that all smaller elements come before pivot and all larger elements come after. The pivot will be in its final position. Recursively apply this to the subarray of smaller elements and the subarray of larger elements.
+It has a partitioning step, in which you pick an element (called a pivot)
+and partition the array so that all smaller elements come before pivot and all
+larger elements come after. The pivot will be in its final position. 
+Recursively apply this to the subarray of smaller elements and the subarray of 
+larger elements.
+
 *** Exercises
-- Write a partition helper function. For choice of pivot, for a basic implementation, we recommend choosing either the first or last element in the subarray. If you need hints, look up the Lumoto partiton scheme. Test this out before moving forward!
+- Write a partition helper function. 
+  For choice of pivot, for a basic implementation, we recommend choosing either
+  the first or last element in the subarray. 
+  If you need hints, look up the Lumoto partiton scheme. 
+  Test this out before moving forward!
 - Implement quicksort iteratively
 - Implement quicksort recursively
 - Identify time complexity
