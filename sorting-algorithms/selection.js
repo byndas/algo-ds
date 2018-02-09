d2ebacea1471ffd1af0b0bb15ef8c818a9aa82b5
Marc Byndas <byndasma@gmail.com>
2018-02-09 05:36:49 +0000

    selection

--- sorting-algorithms/selection.js
+++ sorting-algorithms/selection.js
@@ -23,9 +23,13 @@ sorted portion has now grown:
 - Identify time complexity
 
 Stable Variant
-- Implement as a stable sort - rather than swapping, the minimum value is inserted into the first position and all other items are shifted one to the right. How does this impact performance?
-- Modify function to take comparator function. specify default if not provided (check out native Array.sort comparator function for reference)
-- Use your comparator function to verify that your sort is stable by taking input: [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
+- Implement as a stable sort - rather than swapping, the minimum value is 
+   inserted into the first position and all other items are shifted one to 
+    the right. How does this impact performance?
+- Modify function to take comparator function. specify default if not provided
+   (check out native Array.sort comparator function for reference)
+- Use your comparator function to verify that your sort is stable by taking
+   input: [{value: 15}, {value: 10, order: 1}, {value: 10, order: 2}]
 
 - Implement selection sort for a linked list (you can use your data structure implemention from earlier in the course). How does this impact performance and stability?
 
