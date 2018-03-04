40f2c31a6d82392a1c2c534de83088ea4b1c6370
Marc Byndas <byndasma@gmail.com>
2018-03-04 02:29:23 +0000

    pathCount.js

--- recursion/pathCount.js
+++ recursion/pathCount.js
@@ -20,7 +20,7 @@ Version 2:
 function pathCountV1 (rowCount, columnCount) {
   var pathCount = 0;
   function recurse(i, j) {
-    if (i === rowCount-1 && j === columnCount-1) pathCount++;
+    if (i === rowCount-1 && j === columnCount-1) { pathCount++; }
     else {
       if (i<rowCount-1) recurse(i+1, j);
       if (j<columnCount-1) recurse(i, j+1);
